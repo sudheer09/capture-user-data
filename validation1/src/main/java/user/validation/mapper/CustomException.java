6b36a5f5d724679c6090455f2b7d04a56d97305b
Sud <Sud@192.168.0.7>
2018-07-15 22:08:23 +0530

    Added exception class to this project

---
+++ validation1/src/main/java/user/validation/mapper/CustomException.java
@@ -0,0 +1,28 @@
+package user.validation.mapper;
+
+public class CustomException extends Exception {
+
+	private int status;
+	private String title;
+	private String details;
+	
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDetails() {
+		return details;
+	}
+	public void setDetails(String details) {
+		this.details = details;
+	}
+
+}
